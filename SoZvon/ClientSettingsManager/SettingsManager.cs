97e4568d80b7b5e32c318bf53499596dd4631989
Кирилл Note <kirill.vldk@gmail.com>
2025-09-16 13:43:39 +1000

    daily commit

---
+++ SoZvon/ClientSettingsManager/SettingsManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoZvon.ClientSettingsManager
+{
+    enum TypeSetting { Hotkey, ComboBox, CheckBox }
+
+    record Setting(string Id, TypeSetting Type, string Value);
+    internal class SettingsManager
+    {
+        readonly Main_Thread.IUser user;
+        public SettingsManager(Main_Thread.IUser user)
+        {
+            this.user = user;
+
+        }
+    }
+}
